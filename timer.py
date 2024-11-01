574572d39ffa483de8cd19d543e60f3b903489e4
Yintong Luo <93970419+yintong16@users.noreply.github.com>
2024-10-31 20:46:35 -0700

    finished uart communication experiment

--- timer.py
+++ timer.py
@@ -12,6 +12,7 @@ def check_computation_time(serial_port, baud_rate=9600, start_signal=b'1', stop_
                 if ser.in_waiting > 0:
                     signal = ser.read().strip()
                     if signal == start_signal:
+                        # print("start")
                         start_time = time.time()  # Start the timer
                         break
 
@@ -20,17 +21,20 @@ def check_computation_time(serial_port, baud_rate=9600, start_signal=b'1', stop_
                 if ser.in_waiting > 0:
                     signal = ser.read().strip()
                     if signal == stop_signal:
+                        # print("end")
                         end_time = time.time()  # Stop the timer
                         break
 
             # Calculate and print the elapsed time
             elapsed_time = end_time - start_time
             print(f"Elapsed time: {elapsed_time:.6f} seconds")
+            print(f"Elapsed time: {elapsed_time * 1000:.6f} milliseconds")
+            
     
     except serial.SerialException as e:
         print(f"Error opening serial port: {e}")
 
 # Usage
-serial_port = 'COM8'  # Change to your serial port, e.g., '/dev/ttyUSB0' on Linux
+serial_port = '/dev/tty.usbmodem0E23B5EB1'  # Change to your serial port, e.g., '/dev/ttyUSB0' on Linux
 
 check_computation_time(serial_port)
